84ba62c983a4b0c590772176cb5b94b3a43f46c4
Elis Hirwing <elis@hirwing.se>
2019-06-08 23:01:43 +0200

    publish: Avoid unneeded redirects

--- publish.el
+++ publish.el
@@ -50,9 +50,9 @@
   "<nav>
      <ul>
        <li><a href=\"/\">~elis/</a></li>
-       <li><a href=\"/work\">./work/</a></li>
-       <li><a href=\"/about\">./about/</a></li>
-       <li><a href=\"/blog\">./blog/</a></li>
+       <li><a href=\"/work/\">./work/</a></li>
+       <li><a href=\"/about/\">./about/</a></li>
+       <li><a href=\"/blog/\">./blog/</a></li>
      </ul>
    </nav>")
 
