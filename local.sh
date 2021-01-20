21b058832eb601424f6d9a30f375aca559038aaa
Elis Hirwing <elis@hirwing.se>
2021-01-20 22:33:12 +0100

    local: Improve local test script so one can specify a path to open in browser

--- local.sh
+++ local.sh
@@ -1,12 +1,27 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash -p python3 xdg_utils firefox
 
+#
+# Usage:
+# ./local.sh [optional source or web path]
+#
+# Example:
+# ./local.sh src/blog/hello-world
+#
+
 nix-build default.nix
 
 if test -L result
 then
-   # Sleep a second and then open the browser
-   sleep 1 && xdg-open "http://localhost:8000/" &
+    # Strip src/ from page path and replace .org extension with .html
+    page_path=$(echo $1 | sed -e 's#^src/##' -e 's#.org$#.html#')
 
-   cd result/ && python -m http.server
+    # Sleep a second and then open the browser
+    sleep 1 && xdg-open "http://localhost:8000/$page_path" &
+
+    # Launch web server
+    cd result/ && python -m http.server && cd -
+
+    # Remove symlink
+    rm result
 fi
