9fe2183f91b4127e77ba6f2f0145f0810db40e7d
Elis Hirwing <elis@hirwing.se>
2024-02-24 17:47:34 +0100

    feat(theme): Switch from independent theme to my company theme

--- flake.nix
+++ flake.nix
@@ -1,51 +1,28 @@
 {
   description = "etu/etu.github.io";
 
-  inputs.flake-utils.url = "flake-utils";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    flake-utils.url = "flake-utils";
+    taserud-theme-albatross.url = "github:TaserudConsulting/theme-albatross";
+    taserud-theme-albatross.inputs.flake-utils.follows = "flake-utils";
+  };
 
   outputs = {
     self,
     flake-utils,
     nixpkgs,
     ...
-  }:
+  } @ inputs:
     flake-utils.lib.eachSystem ["x86_64-linux"] (system: let
-      pkgs = import nixpkgs {inherit system;};
+      pkgs = nixpkgs.legacyPackages.${system};
+      tpkgs = inputs.taserud-theme-albatross.packages.${system};
       domain = "elis.nu";
     in {
       formatter = pkgs.alejandra;
 
-      packages.hugo = pkgs.symlinkJoin {
-        name = "hugo-${pkgs.hugo.version}-dart-sass-embedded-${pkgs.dart-sass.version}-bundle";
-
-        buildInputs = [pkgs.makeWrapper];
-        paths = [pkgs.hugo pkgs.dart-sass];
-
-        postBuild = "wrapProgram $out/bin/hugo --prefix PATH : ${pkgs.dart-sass}/bin";
-
-        meta.mainProgram = "hugo";
-      };
-
-      packages.fontawesome = let
-        version = "6.5.1";
-      in
-        pkgs.stdenv.mkDerivation {
-          pname = "fontawesome-free";
-          inherit version;
-
-          src = pkgs.fetchzip {
-            url = "https://use.fontawesome.com/releases/v${version}/fontawesome-free-${version}-web.zip";
-            hash = "sha256-gXXhKyTDC/Q6PBzpWRFvx/TxcUd3msaRSdC3ZHFzCoc=";
-          };
-
-          buildPhase = ":";
-
-          installPhase = ''
-            mkdir -p $out
-
-            cp -vr scss webfonts $out
-          '';
-        };
+      packages.hugo = tpkgs.hugo;
+      packages.theme = tpkgs.theme;
 
       packages.website = pkgs.stdenv.mkDerivation {
         name = domain;
@@ -53,16 +30,16 @@
         src = ./src;
 
         nativeBuildInputs = [
-          self.packages.${system}.hugo
+          tpkgs.hugo
         ];
 
         buildPhase = ''
-          # Install fontawesome resources
-          install -m 644 -D ${self.packages.${system}.fontawesome}/scss/* -t themes/elisnu/assets/scss/fontawesome
-          install -m 644 -D ${self.packages.${system}.fontawesome}/webfonts/* -t themes/elisnu/static/fonts/fontawesome
+          # Install theme
+          mkdir -p themes
+          ln -s ${tpkgs.theme} themes/${tpkgs.theme.theme-name}
 
           # Build page
-          hugo --logLevel debug
+          hugo --logLevel debug --minify
         '';
 
         installPhase = ''
--- local.sh
+++ local.sh
@@ -3,11 +3,8 @@
 
 set -euo pipefail
 
-# Install the fontawesome files
-rm -rf src/themes/elisnu/assets/scss/fontawesome src/themes/elisnu/static/fonts/fontawesome
-mkdir -p src/themes/elisnu/assets/scss/fontawesome src/themes/elisnu/static/fonts/fontawesome
-install -m 644 -D $(nix build .#fontawesome --print-out-paths --no-link)/scss/* -t src/themes/elisnu/assets/scss/fontawesome
-install -m 644 -D $(nix build .#fontawesome --print-out-paths --no-link)/webfonts/* -t src/themes/elisnu/static/fonts/fontawesome
+# Build the theme for the spcified page and place the symlink in the correct location.
+nix build .#theme --out-link src/themes/$(nix eval .#theme.theme-name --raw)
 
 # Navigate to directory
 cd src/
@@ -16,4 +13,4 @@ cd src/
 sleep 1 && xdg-open "http://localhost:1313/" &
 
 # Run pinned version of hugo
-nix run .#hugo -- server --logLevel debug --disableFastRender
+nix run .#hugo -- server --logLevel debug --disableFastRender --minify
